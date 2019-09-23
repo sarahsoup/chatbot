41a51c760a8d0abaabc7f909baa4dc772050c7c2
sarahsoup <campbell.sar@husky.neu.edu>
2019-09-23 11:18:23 -0500

    initialize

---
+++ script.js
@@ -0,0 +1,58 @@
+function selectBad(round,option){
+    const user = d3.select(`#round-${round}`).select('.user');
+
+    // translate to speech bubble
+    // replace if other bad option was previously chosen
+    user.selectAll('div')  
+        .each(function(d,i){
+            const order = d3.select(this).style('order');
+            if(order != '0'){
+                d3.select(this).classed('hidden',true);
+            }else if(this.id == `option-bad${option}`){
+                d3.select(this)
+                    .classed('option',false)
+                    .classed('bubble-user',true)
+                    .style('order',`-${i+1}`);
+            }
+        })
+
+    // give narrator response
+    user.selectAll('.narrator')
+        .classed('hidden',function(){
+            return (this.id == `response-bad${option}`) ? false : true;
+        })
+
+}
+
+function selectGood(round){
+    const user = d3.select(`#round-${round}`).select('.user');
+
+    // translate to speech bubble
+    // replace if bad option was previously chosen
+    user.selectAll('div')
+        .each(function(d,i){
+            // const order = d3.select(this).style('order');
+            // if(order != '0'){
+            //     d3.select(this).style('order','0');
+            // }
+            if(this.id == 'option-good'){
+                d3.select(this)
+                    .classed('option',false)
+                    .classed('bubble-user',true)
+                    .style('order',`-${i+1}`);
+            }else{
+                d3.select(this).classed('hidden',true);
+            }
+        })
+
+    // give narrator response
+    user.selectAll('.narrator')
+        .classed('hidden',function(){
+            return (this.id == 'response-good') ? false : true;
+        })
+    // user.select('.narrator').classed('hidden',true);
+    // user.select('#response-good').classed('hidden',false);
+
+    // show next round
+    d3.select(`#round-${round+1}`).classed('hidden',false);
+}
