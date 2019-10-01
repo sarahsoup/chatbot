d28296b3d45cf9478d729ae026cb76bc2d142037
sarahsoup <campbell.sar@husky.neu.edu>
2019-09-30 18:25:35 -0700

    added header + full script in csv

--- script.js
+++ script.js
@@ -1,6 +1,78 @@
 
-// // align erica portraits
-// d3.select('#round-1')
+d3.csv('./assets/script.csv').then(data => {
+    console.log(data);
+    const rounds = d3.nest()
+        .key(d => d.round)
+        .entries(data);
+
+    console.log(rounds);
+
+    const container = d3.select('.container');
+
+    rounds.forEach((round,i) => {
+        // append erica div
+        const ericaIndex = round.values.map(d => d.name).indexOf('erica');
+        const ericaText = round.values[ericaIndex].option;
+        const ericaRow = container.append('div')
+            .attr('class',(round.key == '1') ? `row round round-${round.key}` : `row round round-${round.key} hidden`)
+            .attr('id',`round-${round.key}-erica`);
+        ericaRow.append('div')
+            .attr('class','col-2 col-lg-1 offset-lg-2')
+            .append('img')
+            .attr('class','erica-square')
+            .attr('src','./assets/erica_square.png');
+        ericaRow.append('div')
+            .attr('class','col-8 col-lg-6')
+            .append('div')
+            .attr('class','erica')
+            .append('div')
+            .attr('class','bubble-erica')
+            .append('p')
+            .html(ericaText.substring(1, ericaText.length-1));
+
+        // append user div
+        const user = container.append('div')
+            .attr('class',(round.key == '1') ? `row round round-${round.key}` : `row round round-${round.key} hidden`)
+            .attr('id',`round-${round.key}-user`)
+            .append('div')
+            .attr('class','col-12 offset-md-2 col-md-8 offset-lg-3 col-lg-6')
+            .append('div')
+            .attr('class','user');
+        if(i < rounds.length-1){
+            user.append('h2')
+                .attr('class','narrator')
+                .html('How would you reply?');
+        }else{
+            user.append('h2')
+                .attr('class','narrator')
+                .html(round.values[0].response.substring(1,round.values[0].response.length-1));
+        }
+        user.selectAll('.response')
+            .data(round.values.filter(d => d.name != 'erica'))
+            .enter()
+            .append('h2')
+            .attr('class','narrator hidden')
+            .attr('id',d => `response-${d.name}`)
+            .html(d => d.response.substring(1,d.response.length-1));
+        user.selectAll('.option')
+            .data(round.values.filter(d => d.name != 'erica'))
+            .enter()
+            .append('div')
+            .attr('class','option')
+            .attr('id',d => `option-${d.name}`)
+            .on('click',d => {
+                if(d.name == 'good'){
+                    selectGood(+round.key);
+                }else{
+                    console.log(d.name.substring(d.name.length-1))
+                    selectBad(+round.key,+d.name.substring(d.name.length-1));
+                }
+            })
+            .append('p')
+            .html(d => d.option.substring(1,d.option.length-1));
+        
+    })
+})
 
 function selectBad(round,option){
     const user = d3.select(`#round-${round}-user`).select('.user');
